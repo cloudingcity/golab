91ba8711be9cb9c809fba8555100403509992afd
Clouding <ghost0436@gmail.com>
2019-10-13 18:38:20 +0800

    add config configure feature

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,24 @@
 package config
 
 import (
+	"bufio"
+	"fmt"
+	"io"
+	"path"
+	"strings"
+
 	"github.com/spf13/viper"
 )
 
-var v *viper.Viper
+var (
+	v          *viper.Viper
+	configPath string
+)
 
 // Load read config with path
 func Load(path string) error {
+	configPath = path
+
 	v = viper.New()
 	v.SetConfigName("golab")
 	v.SetConfigType("yaml")
@@ -21,4 +32,40 @@ func Load(path string) error {
 // Get returns the value associated with key from config
 func Get(key string) string {
 	return v.GetString(key)
+}
+
+// Configure configure and save file
+func Configure(r io.Reader, w io.Writer) error {
+	reader := bufio.NewReader(r)
+
+	if host := readHost(w, reader); host != "" {
+		v.Set("host", host)
+	}
+
+	if token := readToken(w, reader); token != "" {
+		v.Set("token", token)
+	}
+
+	filePath := path.Join(configPath, "golab.yaml")
+	if err := v.WriteConfigAs(filePath); err != nil {
+		return err
+	}
+
+	fmt.Fprintf(w, "\nConfig saved to %s\n", filePath)
+
+	return nil
+}
+
+func readHost(w io.Writer, reader *bufio.Reader) string {
+	fmt.Fprintf(w, "Gitlab Host [%s]: ", Get("host"))
+	host, _ := reader.ReadString('\n')
+
+	return strings.TrimSpace(host)
+}
+
+func readToken(w io.Writer, reader *bufio.Reader) string {
+	fmt.Fprintf(w, "Gitlab Token (scope: api) [%s]: ", Get("token"))
+	token, _ := reader.ReadString('\n')
+
+	return strings.TrimSpace(token)
 }
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,5 +30,43 @@ func TestGet(t *testing.T) {
 		assert.Error(t, err)
 		assert.Equal(t, Get("host"), "https://gitlab.com")
 		assert.Equal(t, Get("token"), "None")
+	})
+}
+
+func TestConfigure(t *testing.T) {
+	t.Run("skip all", func(t *testing.T) {
+		Load(testPath)
+
+		stdin := bytes.NewBufferString("\n\n")
+		stdout := &bytes.Buffer{}
+		Configure(stdin, stdout)
+
+		filePath := path.Join(testPath, "golab.yaml")
+		want := "Gitlab Host [https://abc.com]: Gitlab Token (scope: api) [NjEaWdDcARhzYKdx4fA4]: \nConfig saved to " + filePath + "\n"
+		got := stdout.String()
+
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("enter something", func(t *testing.T) {
+		dir, _ := ioutil.TempDir("", "golab")
+		defer os.RemoveAll(dir)
+
+		Load(dir)
+		stdin := bytes.NewBufferString("https://foo.com\nfaketoken\n")
+		stdout := &bytes.Buffer{}
+		Configure(stdin, stdout)
+
+		filePath := path.Join(dir, "golab.yaml")
+		want := "Gitlab Host [https://gitlab.com]: Gitlab Token (scope: api) [None]: \nConfig saved to " + filePath + "\n"
+		got := stdout.String()
+
+		assert.Equal(t, want, got)
+		assert.FileExists(t, filePath)
+
+		Load(dir)
+
+		assert.Equal(t, Get("host"), "https://foo.com")
+		assert.Equal(t, Get("token"), "faketoken")
 	})
 }
