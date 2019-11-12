ea5efef1cb6384252c8d607f4e7b60d1653169ca
Clouding <ghost0436@gmail.com>
2019-11-12 23:27:54 +0800

    Implement manager

---
+++ internal/gitlab/group/manager_test.go
@@ -0,0 +1,17 @@
+package group
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/xanzy/go-gitlab"
+)
+
+func TestNewManager(t *testing.T) {
+	c := gitlab.NewClient(nil, "foo")
+	c.SetBaseURL("bar")
+
+	m := NewManager(c, nil)
+
+	assert.NotNil(t, m.MergeRequest)
+}
