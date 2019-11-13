642ade5e2a794cb06de7755a4233117627464527
Clouding <ghost0436@gmail.com>
2019-11-13 22:44:47 +0800

    Add own mr open cmd

---
+++ cmd/own_mr_open.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var ownMrOpenCmd = &cobra.Command{
+	Use:   "open [PROJECT-ID] [MR-ID]",
+	Short: "Open a merge request page in the default browser",
+	Args:  cobra.MinimumNArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return globalManager().MergeRequest.Open(args[0], args[1])
+	},
+}
+
+func init() {
+	ownMrCmd.AddCommand(ownMrOpenCmd)
+}
