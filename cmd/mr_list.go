6f5490a0da6709a565b0909b78e62c362d90fa24
Clouding <ghost0436@gmail.com>
2019-11-25 00:15:42 +0800

    Add mr list --limit option

--- cmd/mr_list.go
+++ cmd/mr_list.go
@@ -11,9 +11,10 @@ var mrListCmd = &cobra.Command{
 	Short:   "List merge requests",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		opt := &gitlab.ListProjectMergeRequestsOptions{
-			State:   gitlab.String(mrListFlag.state),
-			OrderBy: gitlab.String("updated_at"),
-			Scope:   gitlab.String(mrListFlag.optionScope()),
+			State:       gitlab.String(mrListFlag.state),
+			OrderBy:     gitlab.String("updated_at"),
+			Scope:       gitlab.String(mrListFlag.optionScope()),
+			ListOptions: mrListFlag.optionList(),
 		}
 		return projectManager().MergeRequest.List(opt, mrListFlag.url)
 	},
@@ -23,6 +24,7 @@ type mrListFlagStruct struct {
 	review bool
 	state  string
 	url    bool
+	limit  int
 }
 
 func (f *mrListFlagStruct) optionScope() string {
@@ -32,10 +34,18 @@ func (f *mrListFlagStruct) optionScope() string {
 	return "all"
 }
 
+func (f *mrListFlagStruct) optionList() gitlab.ListOptions {
+	return gitlab.ListOptions{
+		Page:    1,
+		PerPage: f.limit,
+	}
+}
+
 var mrListFlag *mrListFlagStruct
 
 func init() {
 	mrListFlag = &mrListFlagStruct{}
+	mrListCmd.Flags().IntVarP(&mrListFlag.limit, "limit", "l", 20, "number of merge requests to list (max 100)")
 	mrListCmd.Flags().BoolVarP(&mrListFlag.review, "review", "r", false, "list merge requests assigned to you")
 	mrListCmd.Flags().StringVarP(&mrListFlag.state, "state", "s", "opened", "filter merge requests by state (opened/closed/locked/merged)")
 	mrListCmd.Flags().BoolVarP(&mrListFlag.url, "url", "u", false, "with url column")
--- cmd/own_mr_list.go
+++ cmd/own_mr_list.go
@@ -11,9 +11,10 @@ var ownMrListCmd = &cobra.Command{
 	Short:   "List merge requests created by you",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		opt := &gitlab.ListMergeRequestsOptions{
-			State:   gitlab.String(ownMrListFlag.state),
-			OrderBy: gitlab.String("updated_at"),
-			Scope:   gitlab.String(ownMrListFlag.optionScope()),
+			State:       gitlab.String(ownMrListFlag.state),
+			OrderBy:     gitlab.String("updated_at"),
+			Scope:       gitlab.String(ownMrListFlag.optionScope()),
+			ListOptions: ownMrListFlag.optionList(),
 		}
 		return globalManager().MergeRequest.List(opt, ownMrListFlag.url)
 	},
@@ -23,6 +24,7 @@ type ownMrListFlagStruct struct {
 	review bool
 	state  string
 	url    bool
+	limit  int
 }
 
 func (f *ownMrListFlagStruct) optionScope() string {
@@ -32,10 +34,18 @@ func (f *ownMrListFlagStruct) optionScope() string {
 	return "created_by_me"
 }
 
+func (f *ownMrListFlagStruct) optionList() gitlab.ListOptions {
+	return gitlab.ListOptions{
+		Page:    1,
+		PerPage: f.limit,
+	}
+}
+
 var ownMrListFlag *ownMrListFlagStruct
 
 func init() {
 	ownMrListFlag = &ownMrListFlagStruct{}
+	ownMrListCmd.Flags().IntVarP(&ownMrListFlag.limit, "limit", "l", 20, "number of merge requests to list (max 100)")
 	ownMrListCmd.Flags().BoolVarP(&ownMrListFlag.review, "review", "r", false, "list merge requests assigned to you")
 	ownMrListCmd.Flags().StringVarP(&ownMrListFlag.state, "state", "s", "opened", "filter merge requests by state (opened/closed/locked/merged)")
 	ownMrListCmd.Flags().BoolVarP(&ownMrListFlag.url, "url", "u", false, "with url column")
